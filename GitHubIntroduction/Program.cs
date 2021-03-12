7257ff960b6e53e9db209efe7cd484dcec22e930
KarenBurgos <00043820@uca.edu.sv>
2021-03-12 11:10:56 -0600

    change the function AddNumbers addition by subtraction

--- GitHubIntroduction/Program.cs
+++ GitHubIntroduction/Program.cs
@@ -14,12 +14,12 @@ namespace GitHubIntroduction
             Console.Write("Enter a value for b: ");
             b = Int32.Parse(Console.ReadLine());
 
-            Console.WriteLine($"Adding {a} + {b} equals {AddNumbers(a, b)}");
+            Console.WriteLine($"subtracting {a} - {b} equals {AddNumbers(a, b)}");
         }
 
         public static int AddNumbers(int a, int b)
         {
-            return a + b;
+            return a - b;
         }
     }
 }
